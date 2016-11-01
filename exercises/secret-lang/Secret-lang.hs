66f4cd1db888858f488b602f67431b4f1588c07b
ivanmilevtues <ivanmilevtues@gmail.com>
2016-11-01 13:21:29 +0200

    Everything done without the decoding

--- exercises/secret-lang/Secret-lang.hs
+++ exercises/secret-lang/Secret-lang.hs
@@ -2,29 +2,45 @@ import Data.Char(toLower)
 
 
 isChar :: Char -> Bool
-isChar = undefined
-
+isChar ch
+    |toLower ch >= 'a' && toLower ch <= 'z' = True
+isChar _ = False
 
 isVowel :: Char -> Bool
-isVowel = undefined
+isVowel ch
+    | toLower ch == 'a' = True
+    | toLower ch == 'e' = True
+    | toLower ch == 'u' = True
+    | toLower ch == 'i' = True
+    | toLower ch == 'o' = True
+    | toLower ch == 'y' = True
+isVowel _ = False
 
 
 isConsonant :: Char -> Bool
-isConsonant = undefined
+isConsonant ch
+    | not (isChar ch) = False
+    | isVowel ch = False
+isConsonant _ = True
 
 
 encode :: String -> String
-encode = undefined
-
+encode [] = []
+encode (x:xs)
+    | isConsonant x = x : 'o' : x : (encode xs)
+encode (x:xs) = x : (encode xs)
 
 -- Bonus #1
 encode' :: String -> String
-encode' = undefined
-
+encode' [] = []
+encode' (x:xs)
+    | isConsonant x = x : 'o' : toLower x : (encode xs)
+encode' (x:xs) = x : (encode xs)
 
 -- Bonus #2
 dropN :: Int -> String -> String
-dropN = undefined
+dropN 0 anyString = anyString
+dropN a  (x:xs) = dropN (a - 1) xs
 
 
 --Assume we'll be decoding only valid words
